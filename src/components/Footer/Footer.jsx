5f1823066150b5e29566a383f2d92107919a9f6d
web-joyanta <web.joyanta@gmail.com>
2026-03-26 02:27:21 +0600

    footer UI update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,98 @@
-
+import { FaFacebook, FaGithub, FaReddit } from "react-icons/fa";
+import logoImg from "../../assets/logo.png"
 const Footer = () => {
     return (
-        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-            <nav>
-                <h6 className="footer-title">Services</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Company</h6>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </nav>
-            <nav>
-                <h6 className="footer-title">Legal</h6>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </nav>
+        <footer className='bg-[#001931] shadow-sm'>
+            <hr />
+            <div className='container px-2 py-8 mx-auto'>
+                <div className='flex flex-col items-center text-center'>
+                    <div className='flex gap-2 items-center'>
+                        <img className='w-auto h-7' src={logoImg} alt='' />
+                        <span className="bg-linear-to-br from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent font-bold">HERO.IO</span>
+                    </div>
+
+                    <div className='flex flex-wrap justify-center mt-6 -mx-4 text-white'>
+                        <a
+                            href='#'
+                            className='mx-4 text-sm transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Reddit'
+                        >
+                            {' '}
+                            Home{' '}
+                        </a>
+
+                        <a
+                            href='#'
+                            className='mx-4 text-sm transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Reddit'
+                        >
+                            {' '}
+                            About{' '}
+                        </a>
+
+                        <a
+                            href='#'
+                            className='mx-4 text-sm transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Reddit'
+                        >
+                            {' '}
+                            Teams{' '}
+                        </a>
+
+                        <a
+                            href='#'
+                            className='mx-4 text-sm transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Reddit'
+                        >
+                            {' '}
+                            Privacy{' '}
+                        </a>
+
+                        <a
+                            href='#'
+                            className='mx-4 text-sm transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Reddit'
+                        >
+                            {' '}
+                            Cookies{' '}
+                        </a>
+                    </div>
+                </div>
+
+                <hr className='my-6 border-gray-200 md:my-10 ' />
+
+                <div className='flex flex-col items-center sm:flex-row sm:justify-between text-white'>
+                    <p className='text-sm '>
+                        © 2026 Joyanta. All Rights Reserved.
+                    </p>
+
+                    <div className='flex -mx-2'>
+                        <a
+                            href='#'
+                            className='mx-2  transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Reddit'
+                        >
+                            <FaReddit className='w-5 h-5 fill-current' />
+                        </a>
+
+                        <a
+                            href='#'
+                            className='mx-2  transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Facebook'
+                        >
+                            <FaFacebook className='w-5 h-5 fill-current' />
+                        </a>
+
+                        <a
+                            href='#'
+                            className='mx-2  transition-colors duration-300 hover:text-blue-500 '
+                            aria-label='Github'
+                        >
+                            <FaGithub className='w-5 h-5 fill-current' />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
